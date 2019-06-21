3103e489b5dd00c210da2ee044597f3f141e92ea
MaliuhaSerhii <s.maliuga@prismsolutions.ru>
2019-06-21 08:18:41 +0300

    initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,12 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import "./header.css"
+
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
+  className="Header"
+
   >
     <div
       style={{
